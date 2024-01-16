a72e74cba875bc587e314f22708191ab2c2252c0
Shakermaker001 <142272543+Shakermaker001@users.noreply.github.com>
2024-01-16 15:11:08 -0600

    legend next

--- static/js/logic2.js
+++ static/js/logic2.js
@@ -12,31 +12,32 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
 function chooseColor(coordinates) {
-    if (coordinates <= 10 && coordinates >=-10) return "green";
-    else if (coordinates > 10 && coordinates <=30) return "yellow";
-    else if (coordinates > 30 && coordinates <=50) return "orange";
-    else if (coordinates > 50 && coordinates <=70) return "red";
-    else if (coordinates > 70 && coordinates <=90) return "purple";
+    if (coordinates <= 10 && coordinates >=-10) return "#bfff00";
+    else if (coordinates > 10 && coordinates <=30) return "	#ffff00";
+    else if (coordinates > 30 && coordinates <=50) return "	#ffbf00";
+    else if (coordinates > 50 && coordinates <=70) return "	#ff8000";
+    else if (coordinates > 70 && coordinates <=90) return "#ff4000";
     else return "black";
   }
 // Get the data with d3.
 d3.json(url).then(function(geoJsonData) {
 
   var heatLayer = L.geoJSON(geoJsonData, {
-        pointToLayer: function (feature, coordinate) {
-          return L.circleMarker(coordinate, {
+        pointToLayer: function (feature, latlng) {
+          return L.circleMarker(latlng, {
             radius: feature.properties.mag * 5,
-            fillColor: chooseColor(feature.point.coordinates[2]),
+            fillColor: chooseColor(feature.geometry.coordinates[2]),
             color: '#000',
             weight: 3,
             opacity: 1,
             fillOpacity: 0.8
           });
+        },
+        onEachFeature: function(feature, layer) {
+          layer.bindPopup("<h1>" + feature.properties.title + "</h1>");
         }
+        
       }).addTo(myMap);
-  
-  
-    // Add our marker cluster layer to the map.
-   
-  
+      
+      
   });
