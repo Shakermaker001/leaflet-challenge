bf67f72b225ed3f8172a659057e4b55aed808e89
Shakermaker001 <142272543+Shakermaker001@users.noreply.github.com>
2024-01-17 20:35:07 -0600

    For now

--- static/js/logic2.js
+++ static/js/logic2.js
@@ -17,8 +17,10 @@ function chooseColor(coordinates) {
     else if (coordinates > 30 && coordinates <=50) return "	#ffbf00";
     else if (coordinates > 50 && coordinates <=70) return "	#ff8000";
     else if (coordinates > 70 && coordinates <=90) return "#ff4000";
-    else return "black";
+    else return "#ff0000";
   }
+
+
 // Get the data with d3.
 d3.json(url).then(function(geoJsonData) {
 
@@ -35,9 +37,42 @@ d3.json(url).then(function(geoJsonData) {
         },
         onEachFeature: function(feature, layer) {
           layer.bindPopup("<h1>" + feature.properties.title + "</h1>");
-        }
-        
-      }).addTo(myMap);
-      
-      
-  });
+        },
+  }).addTo(myMap);
+
+  // Set up the legend.
+// Define legend control and add it to the map
+var legend = L.control({ position: 'bottomright' });
+
+legend.onAdd = function (myMap) {
+    var div = L.DomUtil.create('div', 'info legend');
+    // Add legend content here
+    return div;
+};
+
+legend.addTo(myMap);
+
+// Define the legend labels and colors based on altitude range
+function getColor(d) {
+  return d > 10000 ? '#f06b6b' :
+         d > 5000  ? '#f0936b' :
+         d > 2000  ? '#f3ba4e' :
+         d > 1000  ? '#f3db4c' :
+                     '#45b29d';
+}
+
+// Add legend content dynamically based on altitude range
+legend.onAdd = function (myMap) {
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [0, 1000, 2000, 5000, 10000],
+        labels = [];
+
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+
+    return div;
+};
+ });
